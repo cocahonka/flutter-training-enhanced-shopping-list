1fafcab202c601013b3ea1365fd15d7ca6d53163
Ivan <cocahonka@gmail.com>
2024-01-07 19:41:55 +0300

    added ability to delete groceries

--- lib/widgets/groceries_list.dart
+++ lib/widgets/groceries_list.dart
@@ -1,9 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_list_enhanced/extensions/string.dart';
+import 'package:shopping_list_enhanced/models/grocery.dart';
 import 'package:shopping_list_enhanced/scopes/groceries_scope.dart';
 
-class GroceriesList extends StatelessWidget {
+class GroceriesList extends StatefulWidget {
   const GroceriesList({super.key});
+
+  @override
+  State<GroceriesList> createState() => _GroceriesListState();
+}
+
+class _GroceriesListState extends State<GroceriesList> {
+  void _deleteGrocery(Grocery grocery) {
+    final scope = GroceriesScope.of(context, listen: false);
+    scope.deleteGrocery(grocery);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,14 +24,18 @@ class GroceriesList extends StatelessWidget {
       itemCount: groceries.length,
       itemBuilder: (context, index) {
         final grocery = groceries[index];
-        return ListTile(
-          leading: Container(
-            width: 24,
-            height: 24,
-            color: grocery.category.color,
+        return Dismissible(
+          key: ValueKey(grocery.id),
+          onDismissed: (_) => _deleteGrocery(grocery),
+          child: ListTile(
+            leading: Container(
+              width: 24,
+              height: 24,
+              color: grocery.category.color,
+            ),
+            title: Text(grocery.name.capitalize()),
+            trailing: Text(grocery.quantity.toString()),
           ),
-          title: Text(grocery.name.capitalize()),
-          trailing: Text(grocery.quantity.toString()),
         );
       },
     );
