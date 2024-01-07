c5b1631913903693c685a0d0e865c468ec70053c
Ivan <cocahonka@gmail.com>
2024-01-07 19:38:51 +0300

    made up groceries list

--- lib/widgets/groceries_list.dart
+++ lib/widgets/groceries_list.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:shopping_list_enhanced/extensions/string.dart';
+import 'package:shopping_list_enhanced/scopes/groceries_scope.dart';
 
 class GroceriesList extends StatelessWidget {
   const GroceriesList({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final groceries = GroceriesScope.of(context).groceries;
+
+    return ListView.builder(
+      itemCount: groceries.length,
+      itemBuilder: (context, index) {
+        final grocery = groceries[index];
+        return ListTile(
+          leading: Container(
+            width: 24,
+            height: 24,
+            color: grocery.category.color,
+          ),
+          title: Text(grocery.name.capitalize()),
+          trailing: Text(grocery.quantity.toString()),
+        );
+      },
+    );
   }
 }
